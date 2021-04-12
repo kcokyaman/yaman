69260e643b0f08c743166902d04569cc433a13f5
Kursad Cokyaman <kcokyaman@yahoo.com>
2021-04-12 12:06:49 +0300

    Delete pass.sh

--- pass.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Bu script ile AIX uzerindeki kullanıcı sifresi degistirilir.
-pass=`dd if=/dev/urandom bs=16 count=1 2>/dev/null | openssl base64 | sed "s/[=O/\]//g" | cut -b1-16`
-echo "test":$pass|chpasswd
-pwdadm -c test
-echo "test kullanıcısının şifresi değiştirildi: "$pass>>/tmp/pass.log
